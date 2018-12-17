c6b0f0c3861ac4df304302f712d1636b2a92cd62
William Jenner <willjenner29@gmail.com>
2018-12-17 02:00:46 +0000

    removed old db project

--- AmgioDatabase/Engineers.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].[Engineers]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Name] VARCHAR(50) NULL
-)
--- AmgioDatabase/Rota.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Rota]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Engineer_Id] INT NOT NULL, 
-    [Shift] DATETIME NOT NULL, 
-    CONSTRAINT [FK_Rota_ToTable] FOREIGN KEY ([Engineer_Id]) REFERENCES [Engineers]([Id]) 
-)
